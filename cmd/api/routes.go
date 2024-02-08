9364e128a2aca1303a1ebf02e3f973ceddb49d56
liamgluna <liamgluna@gmail.com>
2024-02-08 10:20:55 +0800

    Refactor health check endpoint and handler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -14,7 +14,7 @@ func (app *application) routes() http.Handler {
 	// custom error handler for 405 Method Not Allowed responses
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
-	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/health", app.healthHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/universities", app.createUniversityHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/universities/:id", app.showUniversityHandler)
 
