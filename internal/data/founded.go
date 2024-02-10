6b8ea209273082084be988e78a236f70728323c8
liamgluna <liamgluna@gmail.com>
2024-02-10 22:50:22 +0800

    Add Date type and implement json interfaces

---
+++ internal/data/founded.go
@@ -0,0 +1,42 @@
+package data
+
+import (
+	"errors"
+	"strconv"
+	"time"
+)
+
+var ErrInvalidDateFormat = errors.New("invalid date format")
+
+type Date time.Time
+
+// Implement json.Marshaler and json.Unmarshaler interfaces
+
+
+// Because MarshalJSON() needs to return a byte slice and not
+// modify the receiver, we can use a value receiver for this method.
+func (d Date) MarshalJSON() ([]byte, error) {
+	// format the time.Time value using the desired layout and
+	// wrap it in double quotes before returning it
+	return []byte(strconv.Quote(time.Time(d).Format("2006-01-02"))), nil
+}
+
+// IMPORTANT: Because UnmarshalJSON() needs to modify the
+// receiver, we must use a pointer receiver for this to work correctly. 
+func (d *Date) UnmarshalJSON(jsonValue []byte) error {
+	// try to unmarshal the data into a time.Time
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidDateFormat
+	}
+
+	t, err := time.Parse("2006-01-02", unquotedJSONValue)
+	if err != nil {
+		return ErrInvalidDateFormat
+	}
+
+	// then assign the time.Time value to the Date
+	*d = Date(t)
+
+	return nil
+}
--- internal/data/universities.go
+++ internal/data/universities.go
@@ -10,7 +10,7 @@ type University struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"-"`
 	Name      string    `json:"name"`
-	Founded   int32     `json:"founded"`
+	Founded   Date      `json:"founded"`
 	Location  string    `json:"location"`
 	Campuses  []string  `json:"campuses,omitempty"`
 	Website   string    `json:"website"`
@@ -21,9 +21,10 @@ func ValidateUniversity(v *validator.Validator, university *University) {
 	v.Check(university.Name != "", "name", "must be provided")
 	v.Check(len(university.Name) <= 150, "name", "must not be more than 150 bytes long")
 
-	v.Check(university.Founded != 0, "founded", "must be provided")
-	v.Check(university.Founded >= 1589, "founded", "must be greater than or equal to 1589")
-	v.Check(university.Founded <= int32(time.Now().Year()), "founded", "must be less than or equal to the current year")
+	founded := time.Time(university.Founded)
+	v.Check(!founded.IsZero(), "founded", "must be provided")
+	v.Check(founded.Year() >= 1589, "founded", "must be greater than or equal to 1589")
+	v.Check(founded.Year() <= time.Now().Year(), "founded", "must be less than or equal to the current year")
 
 	v.Check(university.Location != "", "location", "must be provided")
 	v.Check(len(university.Location) <= 500, "location", "must not be more than 500 bytes long")
