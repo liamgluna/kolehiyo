dd88ab7b7914214e59b8093ec119a7b85c1d8766
liamgluna <liamgluna@gmail.com>
2024-02-19 08:11:13 +0800

    Update .gitignore, add build/api target to Makefile, and add vcs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,16 +4,20 @@ import (
 	"context"
 	"database/sql"
 	"flag"
+	"fmt"
 	"log/slog"
 	"os"
 	"strings"
 	"time"
 
 	"github.com/liamgluna/kolehiyo/internal/data"
+	"github.com/liamgluna/kolehiyo/internal/vcs"
 	_ "github.com/lib/pq"
 )
 
-const version = "1.0.0"
+var (
+	version = vcs.Version()
+)
 
 type config struct {
 	port int
@@ -61,7 +65,14 @@ func main() {
 		return nil
 	})
 
+	displayVersion := flag.Bool("version", false, "Display version and exit")
+
 	flag.Parse()
+
+	if *displayVersion {
+		fmt.Printf("Version:\t%s\n", version)
+		os.Exit(0)
+	}
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
