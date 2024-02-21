167d585c65de72327dae421558fff53bdad5bf05
liamgluna <liamgluna@gmail.com>
2024-02-21 22:14:49 +0800

    Redirect root and /api to /api/v0

--- cmd/api/home.go
+++ cmd/api/home.go
@@ -3,9 +3,14 @@ package main
 import "net/http"
 
 func (app *application) homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" || r.URL.Path == "/api"{
+		http.Redirect(w, r, "https://kolehiyo.live/api/v0", http.StatusMovedPermanently)
+		return
+	}
+
 	data := envelope{
-		"message": "Welcome to Kolehiyo, a RESTful API for universities in the Philippines.",
-		"universities": "https://kolehiyo.live/v1/universities",
+		"message":      "Welcome to Kolehiyo, a RESTful API for universities in the Philippines.",
+		"universities": "https://kolehiyo.live/api/v0/universities",
 	}
 
 	err := app.writeJSON(w, http.StatusOK, data, nil)
